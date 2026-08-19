d2670e50f72674732b3160468cb5f6efea40d99d
Ulysses <ulyssesburdeniii@gmail.com>
2026-08-19 11:13:33 -0400

    Added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header>
-            <h1>This is my TODO List APP</h1>
+        <header className="todo-header">
+            <h1>My TODO List</h1>
             <p>Manage your tasks efficiently</p>
             <p>Stay organized and never miss a task</p>
             <p>Boost your productivity with ease</p>
             <p>Keep track of your progress effortlessly</p>
-
         </header>
     );
 };
